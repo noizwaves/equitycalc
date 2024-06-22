4147b8114ace41891632907a24eebeb856ba0916
Adam Neumann <1007983+noizwaves@users.noreply.github.com>
2024-06-22 16:00:23 -0700

    Fix formatting of cents in reports

--- src/report.rs
+++ src/report.rs
@@ -2,5 +2,5 @@ pub mod incr;
 pub mod total;
 
 fn format_currency(cents: i32) -> String {
-    format!("{0}.{1:2>0}", cents / 100, cents % 100)
+    format!("{0}.{1:02}", cents / 100, cents % 100)
 }
